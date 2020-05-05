887e36af42184afa4b5ae990f3a4fcda6b8cd09a
AndreiBalasa <andreibalasa1998@yahoo.com>
2020-05-06 01:43:36 +0300

    Classes and forms created (1st commit)

---
+++ TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Reprezinta una dintre echipe din matchup 
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Reprezinta scorul pentru aceasta echipa
+        /// </summary>
+        public double Score { get; set; }
+        
+        /// <summary>
+        /// Reprezinta matchap-ul precedent din care aceasta echipa a iesit castigatoare
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TournamentTracker/TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// Echipele ce participa in acest meci
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+
+        /// <summary>
+        /// Echipa castigatoare
+        /// </summary>
+        public TeamModel Winner { get; set; }
+
+        /// <summary>
+        /// Runda respectiva
+        /// </summary>
+        public int MatchupRound { get; set; }
+
+
+
+    }
+}
